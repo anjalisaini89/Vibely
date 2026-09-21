56c75de8c98e31bd0b199ad19fb4a82e0cafc357
Anjali Saini <anjalisaini8996@gmail.com>
2026-09-21 21:43:09 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 import path from "path";
-import { fileURLToPath } from "url";  
+import { fileURLToPath } from "url";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -7,6 +7,8 @@ import dotenv from "dotenv";
 import songRoutes from "./routes/songRoutes.js";
 
 dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -23,6 +25,10 @@ app.use(
 );
 
 app.use(express.json());
+app.use(
+  "/uploads",
+  express.static(path.join(__dirname, "uploads"))
+);
 
 /* ================================
    ROOT
